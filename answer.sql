ec9386c131add3b7c40c6e22384f793118a793e4
abdikhafarabdullahi238-a11y <abdikhafarabdullahi238@gmail.com>
2026-09-25 16:19:15 +0300

    Add student table with sample data and update query
    Created a student table and inserted initial data.

--- answer.sql
+++ answer.sql
@@ -18,3 +18,25 @@ SELECT productName, quantityInstock
 FROM products
 ORDER BY buyPrice ASC
 LIMIT 5;
+
+
+USE sales;
+DROP TABLE IF EXISTS student;
+
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    fullName VARCHAR(100),
+    age INT
+);
+
+-- then your INSERT and UPDATE
+INSERT INTO student (id, fullName, age) VALUES
+(1, 'John Doe', 19),
+(2, 'Jane Smith', 22),
+(3, 'Peter Jones', 21);
+
+UPDATE student
+SET age = 20
+WHERE id = 2;
+
+SELECT * FROM student;
